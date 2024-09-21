a227687b9f8f0aa38a7891c8ec86175638c7536e
Avery Broderick <abroderi@rigel.uwaterloo.ca>
2024-09-21 00:01:54 -0400

    Fixed strange zero-flux situation but changing the time range from [0,period] to [-period,0]

--- src/analyses/ProjectSpecific/SgrA/general_riaf_axion_movie.cpp
+++ src/analyses/ProjectSpecific/SgrA/general_riaf_axion_movie.cpp
@@ -272,7 +272,7 @@ int main(int argc, char* argv[])
     for (int j=0; j<frames_per_period(); ++j)
     {
       // std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
-      riaf.set_tobs(j*axion_period/double(frames_per_period()));
+      riaf.set_tobs((j-frames_per_period())*axion_period/double(frames_per_period()));
       riaf.generate_model(params);
 
       riaf.generate_complex_visibilities();
